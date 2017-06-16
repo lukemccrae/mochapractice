92c660b232829d5719a81732fe3e6a6a6753e237
“lukemccrae” <lukemccrae@gmail.com>
2017-06-16 15:52:05 -0600

    made a few tests

---
+++ app.js
@@ -0,0 +1,8 @@
+module.exports = {
+  sayHello: function() {
+    return 'hello'
+  },
+  sayBye: function() {
+    return 'bye'
+  }
+}
---
+++ test/appTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const sayHello = require('../app').sayHello;
+
+describe('App', function() {
+  it('sayHello should return hello', function() {
+    let result = sayHello()
+    assert.equal(result, 'hello')
+  });
+
+  it('sayHello should return type string', function() {
+    let result = sayHello();
+    assert.typeOf(result, 'string ')
+  })
+})
